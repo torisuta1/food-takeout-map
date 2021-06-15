b5863c166d32c17fa3b8181b81f6ec8fd3fb37a8
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-06-15 16:58:53 +0900

    device_routes_set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controller: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy" 
+  end
   root to: "home#index"
 end
