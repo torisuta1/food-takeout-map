86bd22f553d669fca1904003c7d327d5ab67f2ea
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-06-27 01:34:50 +0900

    [add]rspec home_page view test
    home-pageのログイン表示のテストのため

--- spec/views/home_spec.rb
+++ spec/views/home_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe 'home_page' do
     expect(page).to have_content 'サインイン'
     end
   end
+
+  describe 'log_in' do  
+    it 'existing ログイン' do 
+      visit root_path
+    expect(page).to have_content 'ログイン'
+    end
+  end
 end
