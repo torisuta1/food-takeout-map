884554720aa3f4eae86e9740f4f4889ac94e1b0d
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-07-01 19:54:07 +0900

    [add]sign_up mail認証
    サインアップの際のメール認証の追加のため

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => "foodtakeoutmap@gmail.com", #gmailアドレス
+  :password => "railstest", #gmailパスワード
+  :authentication => 'login',
+}
 end
