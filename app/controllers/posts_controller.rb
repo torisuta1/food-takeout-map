25cf426a314bb03f63b560f8cb18568fa07f674b
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-07-24 03:02:10 +0900

    [add]post controller create
     #5 ポストコントローラーの投稿処理実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,23 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = "投稿が完了しました"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def show
+  end
+
+private
+
+  def post_params 
+    params.require(:post).permit(:title, :content)
   end
 end
