fd81098470b63c37f684dee07c792f03cd2ed05c
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-07-24 19:56:40 +0900

    [add]rendered url reconfiguration with js
     #5 レンダー時のjsによるurl変更の追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,6 @@ Rails.application.routes.draw do
     
   } 
 
-  devise_scope :user do
-    get "sign_in", to: "users/sessions#new"
-    get "sign_out", to: "users/sessions#destroy" 
-    get "sign_up", to:"users/registrations#new"
-    post "sign_up", to:"users/registrations#create"
-    post "sign_in", to:"users/sessions#create"
-    get "users/password", to: "users/passwords#new"
-    get "users", to:"users/registrations#edit"
-  end
   root to: "home#index"
   resources :posts, only: [:new, :create, :show]
 end
