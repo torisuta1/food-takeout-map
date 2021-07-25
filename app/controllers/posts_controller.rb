351fba2a0e2eac60c6ad30f6d084f76c7560a2ca
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-07-26 04:16:35 +0900

    [add]posts_list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page])
+  end
 
   def new
     @post = Post.new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,27 @@
-User.all.each do |user|
-  user.posts.create! ([
-    {
-      title: 'テストタイトル',
-      content: 'テストコンテンツ'
-    }
-  ])
-end
+# User.all.each do |user|
+#   user.posts.create! ([
+#     {
+#       title: 'テストタイトル',
+#       content: 'テストコンテンツ'
+#     }
+#   ])
+# end
 
-30.times do |n|
-  Post.create! (
-    {
-    title:"テスト#{n+1}回目",
-    content: "テスト#{n+1}回目の投稿",
-    user_id: '25'
-    }
-  )
+# 30.times do |n|
+#   Post.create! (
+#     {
+#     title:"テスト#{n+1}回目",
+#     content: "テスト#{n+1}回目の投稿",
+#     user_id: '25'
+#     }
+#   )
+# end
+
+5.times do |n|
+  User.create! ([
+    username: "テスト#{n+1}",
+    email: "test#{n+1}@example.com",
+    password: "password",
+    confirmed_at: Time.current
+])
 end
