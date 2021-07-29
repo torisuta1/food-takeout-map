f97b6145c05679fe62267aa0da2ea00b8b573bb7
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-07-29 18:19:29 +0900

    [add]serch_view
     #15 検索機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ApplicationRecord
   belongs_to:genre
   validates :user_id, presence: true
   validates :title, presence: true, length: {maximum: 40}
+
+  def self.search(search, genre)
+    if search.blank? && genre.blank?
+      return Post.includes(:user)
+    elsif search.blank?
+      Post.where(genre_id: genre)
+    else
+      Post.where(['title LIKE ? or content LIKE ? ' , "%#{search}%", "%#{search}%"]).or(Post.where(genre_id: genre)) 
+    end
+  end
 end
