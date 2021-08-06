f6717afa700fecb72642adb174d66daefca23e1c
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-08-07 08:58:50 +0900

    [add]ajax flash
     #27 フラッシュメッセージのajax適用

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,30 +1,33 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_user
+  before_action :set_user
 
   def create
     #  current_user.relationships.create(user_id: current_user.id, follow_id: params[:user_id])
-    @user = User.find(params[:user_id])
-    @relation = Relationship.create(user_id: current_user.id, follow_id: @user.id)
-    end
-     
-    
-    #    flash[:notice] = 'ユーザーをフォローしました'
+    # @user = User.find(params[:user_id])
+    if Relationship.create(user_id: current_user.id, follow_id: @user.id)
+      respond_to do |format|
+        format.js { flash[:danger] = "ユーザーをフォローしました" }
+        end
+      # flash[:notice] = 'ユーザーをフォローしました'
     # else
     #   flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-    # end
-
+    end
+  end
+     
   def destroy
     #  current_user.relationships.find_by(user_id: current_user.id, follow_id: params[:user_id]).destroy
-    @user = User.find(params[:user_id])
+    # @user = User.find(params[:user_id])
     @relation = Relationship.find_by(user_id: current_user.id, follow_id: @user.id)
-    @relation.destroy
-    end
-      
-    #   flash[:notice] = 'ユーザーのフォローを解除しました'
+    if @relation.destroy
+      respond_to do |format|
+        format.js { flash[:danger] = "ユーザーのフォローを解除しました" }
+        end
+      # flash[:notice] = 'ユーザーのフォローを解除しました'
     # else
     #   flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-    # end
+    end
+  end
 
   private
 
