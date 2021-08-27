0313ef5b1f653b533135466606176ee8bb2c99fb
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2021-08-28 01:43:51 +0900

    [clean]sql  n+1
     #63 bulletのインストールとn+1の改善

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: @user.id).page(params[:page])
-    @post = Post.find_by(user_id: params[:id])
+    @posts = Post.includes(:images, :likes, :liked_users).where(user_id: @user.id).page(params[:page])
+    # @post = Post.find_by(user_id: params[:id])
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,11 @@ Rails.application.configure do
   password: ENV["WELCOME_MAILER_PASSWORD"], # gmailパスワード
   authentication: "login",
 }
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
